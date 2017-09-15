5cc0ae338c244a8828c957f1100c1cad7b8bf898
Enrique Gonzalez <enrique@saucelabs.com>
2017-09-15 16:27:52 -0700

    Get topics updated in the last 7 days and check who was the last commenter.
    With this we can check if the author_id belongs to an agent.
    If it doesn't we should send a notification to Slack

--- topics.py
+++ topics.py
@@ -8,37 +8,61 @@ import os
 
 
 URL_BASE = "https://saucelabs.zendesk.com/api/v2/"
+username = os.environ.get('ZENDESK_USERNAME')
+password = os.environ.get('ZENDESK_PASSWORD')
 
 
-def get_topics_json():
-    "Obtain topics with username and password"
+def get_recent_posts_json():
+    "Obtain recent posts sorted by recent activity"
 
-    endpoint =  "help_center/community/topics.json"
+    endpoint =  "help_center/community/posts.json?sort_by=recent_activity"
 
     response = send_request(endpoint, "GET")
 
     load = json.loads(response) 
 
-    return load['topics'] #TODO: Test that checks "topics" is valid
+    return load['posts'] #TODO: Test that checks "posts" is valid
 
-def check_last_update_date(topics):
+def check_last_update_date(posts):
 
-    for topic in topics:
-        last_update = topic['updated_at']
-        print last_update
+    for post in posts:
+        last_update = post['updated_at']
         datetime_last_update = datetime.datetime.strptime(last_update,'%Y-%m-%dT%H:%M:%SZ')
         datetime_now = datetime.datetime.now()
 
-        print datetime_now - datetime_last_update
+        time_difference =  datetime_now - datetime_last_update
 
-def get_agents_ids(username, password):
+        if time_difference.days < 8:
 
-    endpoint = "users.json"
+            print time_difference.days
+            post_id = post['id']
+            print post_id
+            print check_last_commenter(post_id)
+
+def check_last_commenter(id):
+
+    endpoint = "community/posts/{post_id}/comments.json".format(post_id=id)
+
     response = send_request(endpoint, "GET", username, password)
 
     load = json.loads(response)
 
-    return load
+    return load['comments'][0]
+
+
+def get_agents_ids():
+
+    endpoint = "users.json?role=agent"
+    response = send_request(endpoint, "GET", username, password)
+
+    load = json.loads(response)
+
+    agent_ids = []
+
+    for user in load['users']: #TODO: Test that checks "users" is valid
+        user_id = user['id']
+        agent_ids.append(user_id)
+    return agent_ids
 
 def send_request(endpoint, method, username=None, password=None):
 
@@ -58,12 +82,10 @@ def send_request(endpoint, method, username=None, password=None):
     return response
 
 def main():
-    username = os.environ.get('ZENDESK_USERNAME')
-    password = os.environ.get('ZENDESK_PASSWORD')
-    topics = get_topics_json()
-    check_last_update_date(topics)
-    agents = get_agents_ids(username, password)
-    print agents
+    posts = get_recent_posts_json()
+    check_last_update_date(posts)
+    # agents = get_agents_ids()
+    # print agents
 
 if __name__ == '__main__':
     main()
