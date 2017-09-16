2ea6ab26cd0efd9abd5979bb9ff55625747b638d
Enrique Gonzalez <enrique@saucelabs.com>
2017-09-16 11:11:24 -0700

    Convert tabs to spaces

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ import yaml
 logger = logging.getLogger(__name__)
 
 def main():
-	logging.config.dictConfig(yaml.load(open('logconf.yaml', 'r')))
-	logger.info("Caling topics main function")
-	topics.main()
-	logger.info("Function finished. Exiting...")
+    logging.config.dictConfig(yaml.load(open('logconf.yaml', 'r')))
+    logger.info("Caling topics main function")
+    topics.main()
+    logger.info("Function finished. Exiting...")
 
 if __name__ == '__main__':
     main()
